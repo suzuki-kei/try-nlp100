3aa41869d2370424cf2906a157f1e60e291a347e
SUZUKI Kei <todokimasen@gmail.com>
2020-04-27 01:22:10 +0900

    問題 04 を解いた.

--- chapter-1.py
+++ chapter-1.py
@@ -62,6 +62,54 @@ class Chapter1TestCase(unittest.TestCase):
         answer = list(map(len, words))
         self.assertEqual([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9], answer)
 
+    def test04(self):
+        """
+            04. 元素記号
+
+            "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations 
+            Might Also Sign Peace Security Clause. Arthur King Can." という文を
+            単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，
+            それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位
+            置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を
+            作成せよ．
+        """
+        def index_to_chars(index):
+            if index in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
+                return 1
+            else:
+                return 2
+
+        s = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. ' \
+            'New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+        words = _to_words(s)
+        answer = {
+            i: words[i - 1][:index_to_chars(i)]
+            for i in range(1, len(words) + 1)
+        }
+        expected = {
+            1: 'H',
+            2: 'He',
+            3: 'Li',
+            4: 'Be',
+            5: 'B',
+            6: 'C',
+            7: 'N',
+            8: 'O',
+            9: 'F',
+            10: 'Ne',
+            11: 'Na',
+            12: 'Mi',
+            13: 'Al',
+            14: 'Si',
+            15: 'P',
+            16: 'S',
+            17: 'Cl',
+            18: 'Ar',
+            19: 'K',
+            20: 'Ca',
+        }
+        self.assertEqual(expected, answer)
+
 
 if __name__ == '__main__':
     unittest.main()
