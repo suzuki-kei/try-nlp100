3f29c31af23f55ba00a2d245a0ca2bbc7bdffc6f
SUZUKI Kei <todokimasen@gmail.com>
2020-04-27 00:43:20 +0900

    各行が 80 文字以下になるように改行を入れた.

--- chapter-1.py
+++ chapter-1.py
@@ -12,7 +12,8 @@ class Chapter1TestCase(unittest.TestCase):
         """
             00. 文字列の逆順
 
-            文字列”stressed”の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+            文字列 "stressed" の文字を逆に（末尾から先頭に向かって）並べた文字列
+            を得よ．
         """
         s = 'stressed'
         answer = s[::-1]
@@ -22,7 +23,8 @@ class Chapter1TestCase(unittest.TestCase):
         """
             01. 「パタトクカシーー」
 
-            「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+            「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した
+            文字列を得よ．
         """
         s = 'パタトクカシーー'
         answer = ''.join(map(lambda i: s[i - 1], (1, 3, 5, 7)))
@@ -32,7 +34,8 @@ class Chapter1TestCase(unittest.TestCase):
         """
             02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
 
-            「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+            「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタ
+            トクカシーー」を得よ．
         """
         s1 = 'パトカー'
         s2 = 'タクシー'
@@ -43,10 +46,12 @@ class Chapter1TestCase(unittest.TestCase):
         """
             03. 円周率
 
-            "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-            という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+            "Now I need a drink, alcoholic of course, after the heavy lectures 
+            involving quantum mechanics." という文を単語に分解し，各単語の（アル
+            ファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
         """
-        s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+        s = 'Now I need a drink, alcoholic of course, ' \
+            'after the heavy lectures involving quantum mechanics.'
         words = re.compile(r'[a-zA-Z]+').findall(s)
         answer = list(map(len, words))
         self.assertEqual([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9], answer)
