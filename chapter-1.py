8090eaa1a317e885ee8a0bb89791ba3325e85f9c
SUZUKI Kei <todokimasen@gmail.com>
2020-04-27 07:58:03 +0900

    問題 07 を解いた.

--- chapter-1.py
+++ chapter-1.py
@@ -172,6 +172,18 @@ class Chapter1TestCase(unittest.TestCase):
         # 差集合 (x - y)
         self.assertSetEqual({'ad', 'di', 'is', 'se'}, x - y)
 
+    def test07(self):
+        """
+            07. テンプレートによる文生成
+
+            引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．
+            さらに，x=12, y=”気温”, z=22.4として，実行結果を確認せよ．
+        """
+        def f(x, y, z):
+            return '{}時の{}は{}'.format(x, y, z)
+
+        self.assertEqual('12時の気温は22.4', f(12, '気温', 22.4))
+
 
 if __name__ == '__main__':
     unittest.main()
