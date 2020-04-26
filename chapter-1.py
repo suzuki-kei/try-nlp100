3dc4f32d01818d6fe6da9987e91c5ccb43e0a403
SUZUKI Kei <todokimasen@gmail.com>
2020-04-27 00:38:36 +0900

    問題 03 を解いた.

--- chapter-1.py
+++ chapter-1.py
@@ -3,6 +3,7 @@
 # https://nlp100.github.io/ja/ch01.html
 #
 
+import re
 import unittest
 
 class Chapter1TestCase(unittest.TestCase):
@@ -37,6 +38,18 @@ class Chapter1TestCase(unittest.TestCase):
         s2 = 'タクシー'
         answer = ''.join(map(''.join, zip(s1, s2)))
         self.assertEqual('パタトクカシーー', answer)
+
+    def test03(self):
+        """
+            03. 円周率
+
+            "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+            という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+        """
+        s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+        words = re.compile(r'[a-zA-Z]+').findall(s)
+        answer = list(map(len, words))
+        self.assertEqual([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9], answer)
 
 
 if __name__ == '__main__':
