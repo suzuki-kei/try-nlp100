c9a484706bbf3b424480514b088fb9787dca6479
SUZUKI Kei <todokimasen@gmail.com>
2020-05-04 18:00:41 +0900

    第 2 章に取り組むための .py ファイルを作成.

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 
-run:
+run: 1 2
+
+1:
 	python3 chapter-1.py
+
+2:
+	python3 chapter-2.py
 
---
+++ chapter-2.py
@@ -0,0 +1,23 @@
+#
+# 言語処理100本ノック 2020 > 第2章: UNIXコマンド
+# https://nlp100.github.io/ja/ch02.html
+#
+
+import doctest
+import typing
+import unittest
+
+
+def main():
+    ...
+
+
+def test():
+    doctest.testmod()
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
+    test()
+
